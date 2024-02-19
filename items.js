993a9033d8a9997801b58b2c84807b650b24a549
Paloma <paloma.sanchezc@icloud.com>
2024-02-19 16:27:47 +0100

    img

---
+++ items.js
@@ -0,0 +1,88 @@
+const items = [
+  {
+    img: "https://icds.ee/wp-content/uploads/2018/Merkel_beer_Reuters.jpg",
+    alt: "Angela Merkel beer",
+    legende: "Angela Merkel en 2018",
+    id: 1,
+    parllaxSpeed: 0.065
+  },
+  {
+      img: "https://static.dw.com/image/59302591_702.jpg",
+      alt: "Angela Merkel with animal",
+      legende: "Angela Merkel en 2018",
+      id: 2,
+      parllaxSpeed: 0.05
+
+    },
+  
+    // {
+    //   img: "https://media.vanityfair.fr/photos/6151dc113657cac85cbb2c43/16:9/w_2560%2Cc_limit/ABACA_781479_001.jpg",
+    //   alt: "Angela Merkel feeding birds",
+    //   legende: "Angela Merkel en 2018",
+    //   id: 3,
+    //   parllaxSpeed: 0.08
+
+    // },
+  
+    {
+      img: "https://i.guim.co.uk/img/media/d465c37b95a9d60b9502ebb4874fc1f3fa775be6/246_24_2033_1219/master/2033.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=1060f80718168f29bccd9f9ca900ee0d",
+      alt: "Angela Merkel pointing",
+      legende: "Angela Merkel en 2018",
+      id: 4,
+      parllaxSpeed: 0.01
+
+    },
+    {
+      img: "https://i.redd.it/3pza0s1ezba11.jpg",
+      alt: "Angela Merkel submarine",
+      legende: "Angela Merkel en 2018",
+      id: 5,
+      parllaxSpeed: 0.07
+
+    },
+
+    {
+      img: "https://media.vanityfair.fr/photos/6151dc113657cac85cbb2c43/16:9/w_2560%2Cc_limit/ABACA_781479_001.jpg",
+      alt: "Angela Merkel feeding birds",
+      legende: "Angela Merkel en 2018",
+      id: 6,
+      parllaxSpeed: 0.085
+
+    },
+    {
+      img: "https://external-preview.redd.it/nOfutxDlJ85YUOQfcw3XrXqKQSuGc1C3tUq9ViJ3gQQ.jpg?auto=webp&s=2944a2ae9844a47d9cc558369ed8fb80d379ac17",
+      alt: "Angela Merkel phone",
+      legende: "Angela Merkel en 2018",
+      id: 7,
+      parllaxSpeed: 0.06
+
+    },
+    {
+      img: "https://i.guim.co.uk/img/media/4b5051828e8ba7733ee41748564f55c8b16a10a1/0_0_2717_1631/master/2717.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=d669d7bc2ceb11eac4035d9b1b4f52e7",
+      alt: "Angela Merkel sandwich",
+      legende: "Angela Merkel en 2018",
+      id: 8,
+      parllaxSpeed: 0.04
+
+    },
+    // {
+    //   img: "https://www.touteleurope.eu/wp-content/uploads/2020/08/merkel-angela-782.jpg",
+    //   alt: "Angela Merkel en 2018",
+    //   legende: "Angela Merkel en 2018",
+    //   id: 9,
+    //   parllaxSpeed: 0.01
+
+    // },
+    {
+      img: "https://media.nature.com/lw767/magazine-assets/d41586-021-02479-6/d41586-021-02479-6_19659954.jpg",
+      alt: "Angela Merkel chemistry",
+    
+      legende: "Angela Merkel en 2018",
+      id: 10,
+      parllaxSpeed: 0.065
+
+    },
+
+]
+
+export { items };
