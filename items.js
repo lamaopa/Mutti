bb034262f6868213ea3d3e643f00cadf04586e43
Paloma <145389683+p-alom-a@users.noreply.github.com>
2024-11-28 21:27:38 +0100

    Update items.js

--- items.js
+++ items.js
@@ -57,22 +57,8 @@ const items = [
       parllaxSpeed: 0.06
 
     },
-    {
-      img: "https://i.guim.co.uk/img/media/4b5051828e8ba7733ee41748564f55c8b16a10a1/0_0_2717_1631/master/2717.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=d669d7bc2ceb11eac4035d9b1b4f52e7",
-      alt: "Angela Merkel sandwich",
-      legende: "Angela Merkel en 2018",
-      id: 8,
-      parllaxSpeed: 0.04
 
-    },
-    // {
-    //   img: "https://www.touteleurope.eu/wp-content/uploads/2020/08/merkel-angela-782.jpg",
-    //   alt: "Angela Merkel en 2018",
-    //   legende: "Angela Merkel en 2018",
-    //   id: 9,
-    //   parllaxSpeed: 0.01
-
-    // },
+ 
     {
       img: "https://media.nature.com/lw767/magazine-assets/d41586-021-02479-6/d41586-021-02479-6_19659954.jpg",
       alt: "Angela Merkel chemistry",
@@ -83,6 +69,159 @@ const items = [
 
     },
 
+   
+
+ 
+
+    //FOOD
+
+
+    {
+      img: "https://i.guim.co.uk/img/media/4b5051828e8ba7733ee41748564f55c8b16a10a1/0_0_2717_1631/master/2717.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=d669d7bc2ceb11eac4035d9b1b4f52e7",
+      alt: "Holding a original grilled Thuringian sausage",
+      legende: " Holding a Thuringian sausage",
+      place:" 2009 in Erfurt,",
+      category:"food",
+      keyWord:["sandwich","saussage"],
+
+      id: 8,
+      parllaxSpeed: 0.04
+
+    },
+
+    {
+      img: "https://www.telegraph.co.uk/content/dam/news/2016/07/05/89685716_epa05162842_German_Chancellor_Angela_Merkel_smiles_as_she_is_presented_a_Schwarzwaelde_trans_NvBQzQNjv4Bqedg3aqFc7QZ-fcycjJM47UzUm7ZppMwD_IZREn2-zqQ.jpg",
+      alt: "Angela Merkel fish",
+      legende: "Holding a Black Forest cherry cake",
+      place :' During an election campaign for the State Parliament elections in Baden-Württemberg, in Radolfzell, Germany, February 15, 2016.',
+      category:"food",
+      keyWord:["cake"],
+      id: 14,
+     
+    },
+
+    {
+      img: "https://external-preview.redd.it/6TmcDWMoJsxHAv8yEHNVJqoH0UhTmFR84jgdzQU5ofk.jpg?auto=webp&s=8126d211dd80cdef8cf862dd4ea219987eee4c6a",
+      alt: "Angela Merkel kebab",
+      legende: "With the kebab knife in her hand",
+      category:"food",
+      keyWord:["kebab"],
+
+      id: 11,
+     
+    },
+
+    {
+      img: "https://georgianjournal.ge/media/images/georgianews/2015/May/World/angela-fish.jpg",
+      alt: "Angela Merkel fish",
+      legende: "  eating a salted herring" ,
+      place:" during the christening ceremony of ROS 777 fishing trawler 'Mark' in Sassnitz, Mecklenburg-Western Pomerania,May 26, 2015.",
+      category:"food",
+      id: 12,
+     
+    },
+
+
+
+    {
+      img: "https://cdn.prod.www.spiegel.de/images/d00cfd22-0001-0004-0000-000000513356_w960_r1.778_fpx53.84_fpy50.webp",
+      alt: "Angela Merkel fish",
+      legende: "Cuts the first slice of a huge cake ",
+      place :'Economic Conference 2013 of the CDU Economic Council, Berlin',
+      category:"food",
+      keyWord:["cake"],
+      id: 13,
+     
+    },
+
+  
+
+    {
+      img: "https://www.shutterstock.com/editorial/image-editorial/O8D7I131N5TcA550MjI5OQ==/german-chancellor-angela-merkel-eats-original-thuringia-440nw-8219902a.jpg",
+      alt: "Angela Merkel eating saussage",
+      legende: "Eating bratwurst sausage",
+      place :' Thuringian summer festival,2009',
+      category:"food",
+      keyWord:["sandwich","saussage"],
+      id: 14,
+     
+    },
+
+    {
+      img: "https://i.dailymail.co.uk/i/pix/2016/02/29/20/31B5B12800000578-0-image-a-48_1456776997181.jpg",
+      alt: "Angela Merkel eating saussage",
+      legende: "Drinking a beer",
+      place :' during the Trudering festival in Munich, Germany, May 28, 2017.',
+      category:"food",
+      keyWord:["beer","beverage"],
+      id: 15,
+     
+    },
+
+    {
+      img: "https://idsb.tmgrup.com.tr/ly/uploads/images/2021/09/24/thumbs/871x871/147284.jpg",
+      alt: " Angela Merkel drinks a dry ice cocktail",
+      legende: " Drinking a dry ice cocktail",
+      place :' designed by young students of the Junior University in Wuppertal, Germany, May 13, 2019.',
+      category:"food",
+      keyWord:["beverage",],
+      id: 16,
+     
+    },
+
+    {
+      img: "https://img.buzzfeed.com/buzzfeed-static/static/2017-05/28/15/asset/buzzfeed-prod-fastlane-01/sub-buzz-4746-1496001576-2.jpg?downsize=800:*&output-format=auto&output-quality=auto",
+      alt: "Angela Merkel eating saussage",
+      legende: "Drinking a beer",
+      place :' during the Trudering festival in Munich, Germany, May 28, 2017.',
+      category:"food",
+      keyWord:["beer","beverage"],
+      id: 17,
+     
+    },
+
+    
+
+    //Sport
+
+{
+  img: "https://idsb.tmgrup.com.tr/ly/uploads/images/2021/09/24/thumbs/871x871/147271.jpg",
+  alt: " plays with a handball",
+  legende: "Playing with handball",
+  place :'  she received the German national handball team, in Berlin, Germany, April 8, 2019.',
+  category:"sport",
+  keyWord:["ball",],
+  id: 16,
+ 
+},
+
+{
+  img: "https://preview.redd.it/20i4blhda1y21.jpg?auto=webp&s=6b9a07e9a3e3c071e2da47d6b8515f8ff23faeff",
+  alt: "  juggle a table tennis ball on a hairdryer",
+  legende: " juggle a table tennis ball on a hairdryer",
+  place :'  Wuppertal Junior University, Germany. 09th May, 2019',
+  category:"sport",
+  keyWord:["ping-pong",],
+  id: 17,
+ 
+},
+
+    //Technology
+
+    {
+      img: "https://www.parismatch.com/lmnr/f/webp/r/1716,1144,000000,forcex,center-middle/img/var/pm/public/media/image/2022/03/18/02/Angela-Merkel-en-visite-au-Centre-des-astronautes-europeens-a-Cologne-le-18-mai-2016_7.jpg?VersionId=f65KXz4Py6hP7r2X0KtDT7bedccofc8a",
+      alt: "  juggle a table tennis ball on a hairdryer",
+      legende: " juggle a table tennis ball on a hairdryer",
+      place :'  visiting the European Astronaut Center in Cologne on May 18, 2016. Sascha Schurmann/Pool/Reuter',
+      category:"technology",
+      keyWord:["",],
+      id: 18,
+     
+    },
+
 ]
+
+
+
 
 export { items };
