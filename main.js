82599c474082457b5fc6cf57a420c40cdfe5a459
Paloma <145389683+p-alom-a@users.noreply.github.com>
2024-11-29 11:34:23 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,64 +4,69 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     // Adjust line breaks for responsiveness
-    function adjustLineBreaks() {
-        const responsiveBrs = document.querySelectorAll('.responsive-br');
-        // const responsiveBrs2 = document.querySelectorAll('.responsive-br2');
-        // const responsiveBrs3 = document.querySelectorAll('.responsive-br3');
-        // const responsiveBrs4 = document.querySelectorAll('.responsive-br4');
+    // function adjustLineBreaks() {
+    //     const responsiveBrs = document.querySelectorAll('.responsive-br');
+    //     // const responsiveBrs2 = document.querySelectorAll('.responsive-br2');
+    //     // const responsiveBrs3 = document.querySelectorAll('.responsive-br3');
+    //     // const responsiveBrs4 = document.querySelectorAll('.responsive-br4');
 
 
-        const screenWidth = window.innerWidth;
+    //     const screenWidth = window.innerWidth;
 
    
-        responsiveBrs.forEach(br => {
-            if (screenWidth <= 312 && screenWidth >= 576) { 
-                br.classList.add('hidden-br');
-            } else {
-                br.classList.remove('hidden-br');
-            }
-        });
+    //     responsiveBrs.forEach(br => {
+    //         if (screenWidth <= 312 && screenWidth >= 576) { 
+    //             br.classList.add('hidden-br');
+    //         } else {
+    //             br.classList.remove('hidden-br');
+    //         }
+    //     });
         
        
 
         
-    }
+    // }
 
-    adjustLineBreaks();
-    window.addEventListener('resize', adjustLineBreaks);
+    // adjustLineBreaks();
+    // window.addEventListener('resize', adjustLineBreaks);
 
 
 
     
 
     // Text appearance landing page
-    gsap.registerPlugin(ScrollTrigger);
+//---- MUTTI EFFECT
 
-    const splitTypes = document.querySelectorAll('.reveal-type');
 
-    splitTypes.forEach((char, i) => {
-        const bg = char.dataset.bgColor;
-        const fg = char.dataset.fgColor;
 
-        const text = new SplitType(char, { types: 'chars' });
 
-        gsap.fromTo(text.chars,
-            {
-                color: bg,
-            },
-            {
-                color: fg,
-                duration: 0.3,
-                stagger: 0.02,
-                scrollTrigger: {
-                    trigger: char,
-                    start: 'top 80%',
-                    end: 'top 20%',
-                    scrub: true,
-                    toggleActions: 'play play reverse reverse'
-                }
-            });
-    });
+const splitTypes = document.querySelectorAll(".reveal-type");
+
+splitTypes.forEach((char, i) => {
+  const bg = char.dataset.bgColor;
+  const fg = char.dataset.fgColor;
+
+  const text = new SplitType(char, { types: "words" }); // Découpe en mots au lieu de caractères
+
+  gsap.fromTo(
+    text.words,
+    {
+      color: bg
+    },
+    {
+      color: fg,
+      duration: 0.3,
+      stagger: 0.02,
+      scrollTrigger: {
+        trigger: char,
+        start: "top 80%",
+        end: "top 20%",
+        scrub: true,
+        toggleActions: "play play reverse reverse"
+      }
+    }
+  );
+});
 });
 
 
